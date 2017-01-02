daecffdd5460eee49115a0ab0433f9da85d5e49c
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-01-02 22:08:58 +0600

    Adds package parsing and pretty printing method

--- parse-apt-log.rb
+++ parse-apt-log.rb
@@ -7,8 +7,29 @@ include AptLogToHash
 def find_in_commands(content_hash_arr, search_term)
   return [] if search_term.empty?
   content_hash_arr.select do |hash|
-    hash if hash["Commandline"].match(search_term)
+    hash if hash["Commandline"].match(search_term) if hash["Commandline"]
   end
+end
+
+# This method takes a string from apt-log and returns only the package names from it
+def scan_package_names(str_with_package_names)
+  str_with_package_names.split('), '). # first split the individual pkages
+    map { |part| part.partition(' ')[0] }. # then for each splitted part, pick the first item i.e name
+    join(' ') # then join the array items with space
+end
+
+# This method takes a section hash and outputs with usabled format
+def pretty_output(section_hash)
+  raise ArgumentError.new('Invalid argument: Need a hash') unless section_hash.class == Hash
+  raise ArgumentError.new('Invalid argument: Not an apt-section hash') unless section_hash.key?("Commandline")
+  puts "Full command: " + section_hash["Commandline"]
+  puts "User: " + section_hash["Requested-By"]
+  puts "Package change details:"
+  puts "Installed: " + scan_package_names(section_hash["Install"]) if section_hash.key?("Install")
+  puts "Upgraded: " + scan_package_names(section_hash["Upgrade"]) if section_hash.key?("Upgrade")
+  puts "Reinstalled: " + scan_package_names(section_hash["Reinstall"]) if section_hash.key?("Reinstall")
+  puts "Removed: " + scan_package_names(section_hash["Remove"]) if section_hash.key?("Remove")
+  puts "Purged: " + scan_package_names(section_hash["Purge"]) if section_hash.key?("Purge")
 end
 
 def read_apt_logs
@@ -16,8 +37,16 @@ def read_apt_logs
 end
 
 if $0 == __FILE__
-  file_content = read_file('history.log')
+  print "Enter the command or package name to search: "
+  search_term = gets.chomp
+
+  file_content = read_file('../history.log.1')
   content_hash = parse_into_hash(file_content)
-  p find_in_commands(content_hash, 'ttf-mscorefonts-installer')
-  p find_in_commands(content_hash, '')
+  found_results = find_in_commands(content_hash, search_term)
+
+  puts "Found #{found_results.size} results\n"
+  found_results.each do |result|
+    pretty_output(result)
+    puts
+  end
 end
--- spec/parse-apt-log-spec.rb
+++ spec/parse-apt-log-spec.rb
@@ -46,6 +46,43 @@ RSpec.describe "#find_in_commands method" do
   end
 end
 
+RSpec.describe "#pretty_output method" do
+  let(:section_hash) do {"Start-Date"=>"2016-12-19  21:14:55",
+    "Commandline"=>"apt install nautilus-share pantheon",
+    "Requested-By"=>"anwar (1000)",
+    "Install"=>"nautilus-share:amd64 (0.7.3-2ubuntu1)",
+    "End-Date"=>"2016-12-19  21:15:17"}
+  end
+
+  it "takes a hash as an argument" do
+    expect { pretty_output }.to raise_error(ArgumentError, /0 for 1/)
+    expect { pretty_output([]) }.to raise_error(ArgumentError, /Invalid argument:/)
+    expect { pretty_output('') }.to raise_error(ArgumentError, /Invalid argument:/)
+  end
+
+  it "takes a hash containing 'Commandline' key" do
+    expect { pretty_output({}) }.to raise_error(ArgumentError, /Not an apt-section hash/)
+    expect { pretty_output(section_hash) }.not_to raise_error
+  end
+end
+
+RSpec.describe "#scan_package_names" do
+  let(:input_string) { "libdevmapper-dev:amd64 (2:1.02.110-1ubuntu10, automatic), libselinux1-dev:amd64 (2.4-3build2, automatic)" }
+  let(:purge_line) { "linux-headers-4.9.0-040900:amd64 (4.9.0-040900.201612111631), linux-headers-4.9.0-040900-generic:amd64 (4.9.0-040900.201612111631)" }
+  let(:reinstall_line) { "seahorse:amd64 (3.18.0-2ubuntu1), gnome-online-accounts:amd64 (3.18.3-1ubuntu2)" }
+
+  it "takes a string as input" do
+    expect { scan_package_names }.to raise_error(ArgumentError, /0 for 1/)
+  end
+
+  it "returns the package names in a string separated by space" do
+    expect(scan_package_names(input_string)).to eq "libdevmapper-dev:amd64 libselinux1-dev:amd64"
+    expect(scan_package_names("california:amd64 (0.4.0-0ubuntu5)")).to eq "california:amd64"
+    expect(scan_package_names(purge_line)).to eq "linux-headers-4.9.0-040900:amd64 linux-headers-4.9.0-040900-generic:amd64"
+    expect(scan_package_names(reinstall_line)).to eq "seahorse:amd64 gnome-online-accounts:amd64"
+  end
+end
+
 RSpec.describe "#read_apt_logs" do
   it "reads apt log files from /var/log/apt dir" do
     allow(self).to receive(:read_file)
